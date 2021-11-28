5eace7be6c08970b54f4395c16aa77ee21d25112
samantha frangi <sefrangi13@gmail.com>
2021-11-27 22:58:11 -0500

    adds show function

--- server.js
+++ server.js
@@ -6,18 +6,42 @@ const pokemon = require('./models/pokemon.js');
 
 const PORT = 3000;
 
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: false})); 
+
 // index
 app.get('/pokemon', (req, res) => {
   res.render('index.ejs',  {pokemon});
 });
 
+// new
+app.get('/pokemon/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+// delete
+
+// update
+
+// create
+app.post('/pokemon', (req,res) => {
+  req.body.id = pokemon.length + 1;
+  pokemon.push(req.body)
+  res.redirect('/pokemon')
+});
+
+// edit 
+
+
 // show
 app.get('/pokemon/:id', (req, res) => {
   const foundPoke = pokemon.find(function (pokemon) {
-    return pokemon.id === Number(req.params.id)
-  })
-  res.render('show.ejs', {foundPoke})
+  return pokemon.id === req.params.id
+ })
+ res.render('show.ejs', {foundPoke}) 
 })
+
 
 // listener
 app.listen(PORT, () => console.log('its alive!!!'));
